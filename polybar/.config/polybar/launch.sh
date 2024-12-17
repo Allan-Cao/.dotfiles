49c76e2da0c7d4301436d89a520609cfa7dc94de
Allan Cao <allan@allancao.ca>
2024-12-17 03:11:30 -0700

    Adds polybar to second monitor to display i3 status

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,7 +2,17 @@
 
 polybar-msg cmd quit
 
-echo "---" | tee -a /tmp/polybarDesktop.log
+# https://github.com/polybar/polybar/issues/763
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "---" | tee -a /tmp/polybarDesktop.log /tmp/polybarSecond.log
 polybar desktop 2>&1 | tee -a /tmp/polybarDesktop.log & disown
+polybar secondmonitor 2>&1 | tee -a /tmp/polybarSecond.log & disown
 
 echo "Bars launched..."
